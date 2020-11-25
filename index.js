a02e6260e7f107fc952f2d9cd8131af5f5502a7f
uharston <uharston@gmail.com>
2020-11-25 13:41:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+// let array = [2, 3, 4, 5, 6, 7]
 function findMinAndRemove(array){
+    let min = array[0]
+    let index; 
+    array.forEach( (e, i) => {
+        if( e < min ) {
+            min = e 
+            index = i 
+        }
+    })
+    array.splice(index, 1)
+    return min 
 
 }
 
 function selectionSort(array){
-
+    let sorted = [] 
+    while(array.length !== 0 ) {
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted 
 }
